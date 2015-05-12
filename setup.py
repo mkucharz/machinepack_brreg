8c717be614658d4c3610c88ce96dc0badd029cc8
Maciej Kucharz <maciek@kucharz.net>
2015-05-13 01:55:38 +0200

    Fixes.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup
-from machinepack_brreg.config import CONFIG
+
+from machinepack_config import CONFIG
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
